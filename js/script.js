0edee1ed6cb38a99d4fc2fd818c5b4fabae38bc5
kaua Mascarenhas <kauamascarenhasscoutinho@gmail.com>
2024-02-07 09:18:27 -0200

    mudanças na respansividade

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const botão = document.querySelector(".botão");
 const nav = document.querySelector(".nav");
+const navlist = document.querySelector(".nav-list")
 
 botão.addEventListener("click", () => nav.classList.toggle("active"));
