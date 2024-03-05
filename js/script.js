8003f3071b5336b4a6739ed56735da0be01fd7da
kaua Mascarenhas <kauamascarenhasscoutinho@gmail.com>
2024-03-05 17:43:07 -0300

    mudanças

--- js/script.js
+++ js/script.js
@@ -16,4 +16,72 @@ revelar.reveal('.left',
     distance:'90px'
 })
 
+//-------------------------------------------//
+           //--Tecnologias--//
 
+revelar.reveal('.icons-div-one',
+{
+  duration:2000,
+  distance:'90px',
+  delay:500
+})
+
+revelar.reveal('.icons-div-two',
+{
+  duration:2000,
+  distance:'90px',
+  delay:1000
+})
+
+revelar.reveal('.icons-div-tre',
+{
+  duration:2000,
+  distance:'90px',
+  delay:1500
+})
+
+revelar.reveal('.icons-div-for',
+{
+  duration:2000,
+  distance:'90px',
+  delay:2000
+})
+
+revelar.reveal('.icons-div-six',
+{
+  duration:2000,
+  distance:'90px',
+  delay:2500
+})
+
+revelar.reveal('.icons-div-rever',
+{
+  duration:2000,
+  distance:'90px',
+  delay:3000
+})
+
+//----------------------------------------------------//
+
+revelar.reveal('.card-one',
+{
+  duration:2000,
+  distance:'90px',
+  delay:500
+})
+
+revelar.reveal('.card-two',
+{
+  duration:2000,
+  distance:'90px',
+  delay:1000
+})
+
+revelar.reveal('.card-tre',
+{
+  duration:2000,
+  distance:'90px',
+  delay:1500
+})
+
+//------------------------------------------------//
