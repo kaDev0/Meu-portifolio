d8c06f5e2d884fd8316ae3a4899c50dc6006aa31
kaua Mascarenhas <kauamascarenhasscoutinho@gmail.com>
2024-03-06 18:53:08 -0300

    implementação

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ botão.addEventListener("click", () => {
   document.body.classList.toggle('body-no-scroll');
 });
 
- // pagina //
+//-----------------------------------------------//
+
+                // pagina //
 
  window.revelar = ScrollReveal({reset:true})
 
@@ -16,6 +18,102 @@ revelar.reveal('.left',
     distance:'90px'
 })
 
+//---------------------------------------------//
+        
+           //--seção principal--//
+
+ revelar.reveal('.img-page',
+ {
+    duration:2000,
+    distance:'90px',
+    delay:300
+ })
+
+ revelar.reveal('.nome',
+{
+    duration:2000,
+    distance:'90px',
+    delay:600
+})
+
+ revelar.reveal('.anime',
+{
+    duration:2000,
+    distance:'90px',
+    delay:900
+})
+
+revelar.reveal('.descrição',
+{
+    duration:2000,
+    distance:'90px',
+    delay:1200
+})
+
+//---------------------------------------------//
+
+           //--icons--//
+
+ revelar.reveal('.icons-anime',
+ {
+  duration:2000,
+  distance:'90px',
+  delay:500
+ })
+
+ revelar.reveal('.icons-anime-one',
+ {
+  duration:2000,
+  distance:'90px',
+  delay:900
+ })
+
+revelar.reveal('.icons-anime-two',
+{
+  duration:2000,
+  distance:'90px',
+  delay:1400
+})
+
+revelar.reveal('.icons-anime-tre',
+{
+  duration:2000,
+  distance:'90px',
+  delay:1700
+})
+
+//----------------------------------------------//
+
+                //--botão--//
+
+
+revelar.reveal('.botão-seção',
+{
+ duration:2000,
+ distance:'90px',
+ delay:2000
+})
+
+
+//---------------------------------------------//
+
+           //--titulo--//
+
+revelar.reveal('.paragrafo',
+{
+    duration:2000,
+    distance:'90px',
+    delay:500
+})
+
+revelar.reveal('.paragrafo-two',
+{
+    duration:2000,
+    distance:'90px',
+    delay:1200
+})
+
+
 //-------------------------------------------//
            //--Tecnologias--//
 
@@ -23,65 +121,92 @@ revelar.reveal('.icons-div-one',
 {
   duration:2000,
   distance:'90px',
-  delay:500
+  delay:800
 })
 
 revelar.reveal('.icons-div-two',
 {
   duration:2000,
   distance:'90px',
-  delay:1000
+  delay:1300
 })
 
 revelar.reveal('.icons-div-tre',
 {
   duration:2000,
   distance:'90px',
-  delay:1500
+  delay:1900
 })
 
 revelar.reveal('.icons-div-for',
 {
   duration:2000,
   distance:'90px',
-  delay:2000
+  delay:2400
 })
 
 revelar.reveal('.icons-div-six',
 {
   duration:2000,
   distance:'90px',
-  delay:2500
+  delay:3100
 })
 
 revelar.reveal('.icons-div-rever',
 {
   duration:2000,
   distance:'90px',
-  delay:3000
+  delay:3800
 })
 
 //----------------------------------------------------//
+
+                  //--card dos projetos--//
+
+revelar.reveal('.carde-one',
+{
+  duration:2000,
+  distance:'90px',
+  delay:300
+})
+
+revelar.reveal('.carde-two',
+{
+  duration:2000,
+  distance:'90px',
+  delay:600
+})
+
+revelar.reveal('.carde-tre',
+{
+  duration:2000,
+  distance:'90px',
+  delay:1200
+})
+
+//------------------------------------------------//
+
+              //--card tela principal--//
+
 
 revelar.reveal('.card-one',
 {
   duration:2000,
   distance:'90px',
-  delay:500
+  delay:800
 })
 
 revelar.reveal('.card-two',
 {
   duration:2000,
   distance:'90px',
-  delay:1000
+  delay:1500
 })
 
 revelar.reveal('.card-tre',
 {
   duration:2000,
   distance:'90px',
-  delay:1500
+  delay:1900
 })
 
-//------------------------------------------------//
